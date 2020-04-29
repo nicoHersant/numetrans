8a1c6abd808cb3a548d61ea2085531593db166b9
NicoH <nicolas.hersant@artwai.com>
2020-04-29 17:11:57 +0200

    update slider

--- assets/js/others/slider.js
+++ assets/js/others/slider.js
@@ -1,11 +1,11 @@
 function activateSlider(name, nb_slides, auto = false, start = 0){
     if (auto == false){
-        timing = 7000
+        auto = 7000
     }
     ob = new Swipe(document.getElementById(name), {
         startSlide: start,
         speed: 800,
-        auto: timing,
+        auto: auto,
         continuous: true,
         disableScroll: false,
         stopPropagation: false,
