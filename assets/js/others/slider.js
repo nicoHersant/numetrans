6d6618b6875e02cb98ad4f8a8c04e0a29be53f9d
NicoH <nicolas.hersant@artwai.com>
2020-05-12 17:33:13 +0200

    merge header and styles

--- assets/js/others/slider.js
+++ assets/js/others/slider.js
@@ -1,11 +1,11 @@
-function activeSlider(name, auto = false){
+function activateSlider(name, auto = false){
     if (auto == false){
         auto = 7000
     }
     ob = new Swipe(document.getElementById(name), {
         startSlide: 0,
         speed: 800,
-        auto: auto,
+        auto: 7000,
         continuous: true,
         disableScroll: false,
         stopPropagation: false,
