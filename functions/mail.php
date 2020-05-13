e0ba55ba638df770b936fc12f2bc6bc8a4136a40
NicoH <nicolas.hersant@artwai.com>
2020-05-13 16:41:48 +0200

    update footer

--- functions/mail.php
+++ functions/mail.php
@@ -2,16 +2,26 @@
 
 // mail ( string $to , string $subject , string $message [, mixed $additional_headers [, string $additional_parameters ]] )
 
-$to      = 'nicolas.hersant@gmail.com';
-$subject = 'test email';
-$message = 'Hi ! this is a test.';
-$headers = array(
-    'From'          => 'numelops@gmail.com',
-    'Reply-To'      => 'nicolas.hersant@gmail.com',
-    'Content-type'  => 'text/plain',
-);
+if( $_POST['name'] ){
+  $name = $_POST['name'] ;
+  $email = $_POST['email'] ? $_POST['email'] : die();
+  $message = $_POST['message'] ?$_POST['message']  : die();
 
-$success = mail($to, $subject, $message, $headers);
-if (!$success) {
-    $errorMessage = error_get_last()['message'];
+  $to      = 'nicolas.hersant@gmail.com';
+  $subject = 'email from numelops.com form';
+  $message = $message;
+  $headers = array(
+    'From'          => 'numelops@gmail.com',
+    'Reply-To'      => ,
+    'Content-type'  => 'text/plain',
+  );
+  var_dump($to, $subject, $message, $headers);
+  $success = mail($to, $subject, $message, $headers);
+  if (!$success) {
+    $log  = "Infos: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
+            "Error: ".error_get_last()['message'].PHP_EOL.
+            "User: ".$name." ".$email.PHP_EOL.
+            "-------------------------".PHP_EOL;
+    file_put_contents('../logs/log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+  }
 }
