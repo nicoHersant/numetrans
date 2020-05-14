ac40359f2659a040202e8d256c4d123b8cdcd3c1
NicoH <nicolas.hersant@artwai.com>
2020-05-14 11:27:46 +0200

    set error and success messages

--- functions/mail.php
+++ functions/mail.php
@@ -1,27 +1,34 @@
 <?php
 
+include_once '../functions/check-text-input.php';
+
 // mail ( string $to , string $subject , string $message [, mixed $additional_headers [, string $additional_parameters ]] )
-
-if( $_POST['name'] ){
-  $name = $_POST['name'] ;
-  $email = $_POST['email'] ? $_POST['email'] : die();
-  $message = $_POST['message'] ?$_POST['message']  : die();
-
+if( !empty( $_POST ) ){
+  // récupération des variables
+  $name     = $_POST['name'] ? checkInput($_POST['name']) : exit;
+  $email    = $_POST['email'] ? checkInput($_POST['email']) : exit;
+  $message  = $_POST['message'] ? checkInput($_POST['message']) : exit;
+  // constitution du mail
   $to      = 'nicolas.hersant@gmail.com';
   $subject = 'email from numelops.com form';
-  $message = $message;
   $headers = array(
     'From'          => 'numelops@gmail.com',
-    'Reply-To'      => ,
+    'Reply-To'      => $email,
     'Content-type'  => 'text/plain',
   );
-  var_dump($to, $subject, $message, $headers);
+  // envoi du mail
   $success = mail($to, $subject, $message, $headers);
+  // gestion des erreurs et redirection
   if (!$success) {
     $log  = "Infos: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
             "Error: ".error_get_last()['message'].PHP_EOL.
             "User: ".$name." ".$email.PHP_EOL.
             "-------------------------".PHP_EOL;
     file_put_contents('../logs/log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+    header("Location: /?status=error");
+    exit;
+  }else{
+    header("Location: /?status=success");
+    exit;
   }
 }
